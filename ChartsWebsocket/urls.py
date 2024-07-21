dae6cd0eea7b30fb759d3b8b9462c7e14a52d1dc
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-07-21 20:15:24 +0300

    Fix app_name

--- ChartsWebsocket/urls.py
+++ ChartsWebsocket/urls.py
@@ -17,8 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-appname = "graph"
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('graph.urls'))
