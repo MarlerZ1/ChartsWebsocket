60b76dd29b6e7a1193befc5fd58bd8d8e47a15ae
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-07-21 19:47:24 +0300

    Add charts

--- graph/views.py
+++ graph/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'graph/index.html', context={"text": "Testing2"})
+    return render(request, 'graph/index.html', context={"text": "New Value"})
--- static/main.js
+++ static/main.js
@@ -1,8 +1,34 @@
+let ctx = document.querySelector('#chart')
+
+let graphData = {
+    type: 'line', data: {
+        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'], datasets: [{
+            label: 'Simple Dynamic Graph',
+            data: [0, 0, 0, 0, 0, 0, 0],
+            fill: false,
+            borderColor: 'rgb(75,192,192)',
+            tension: 0.1
+        }]
+    }
+}
+
+let chart = new Chart(ctx, graphData)
+
+
 let socket = new WebSocket('ws://localhost:8000/ws/graph/')
 
-socket.onmessage = function (event){
+socket.onmessage = function (event) {
     let djangoData = JSON.parse(event.data)
-    console.log(djangoData)
-
     document.querySelector('#app').innerText = djangoData.value
+
+    let newGraphData = graphData.data.datasets[0].data
+    newGraphData.shift()
+    newGraphData.push(djangoData.value)
+    graphData.data.datasets[0].data = newGraphData
+
+    let newGraphLabels = graphData.data.labels
+    newGraphLabels.shift()
+    newGraphLabels.push(djangoData.day)
+    graphData.data.labels = newGraphLabels
+    chart.update()
 }
