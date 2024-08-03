bdc81dd13ef109c8a7dd801b16319a68257c9892
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-08-03 16:02:50 +0300

    Update settings.py

--- ChartsWebsocket/settings.py
+++ ChartsWebsocket/settings.py
@@ -91,7 +91,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)]
+            'hosts': [('redis', 6379)]
         }
     }
 }
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
